fefd9592e0e1002e7e1e5cc8379b503393ddbd79
donhk <hk.fred.0@gmail.com>
2018-10-19 10:57:02 -0500

    baseline files

---
+++ src/main/java/ninja/donhk/boot/Main.java
@@ -0,0 +1,11 @@
+package ninja.donhk.boot;
+
+import ninja.donhk.controllers.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+        window.start(args);
+    }
+}
